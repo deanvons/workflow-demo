2f7e887798a519a9faa22fcffa19c767265f94fe
deanvons <deanvons@gmail.com>
2024-10-10 13:07:22 +0200

    feat: added various tests see demo vids

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-import { add } from "./calculator";
+import { add, divide } from "./calculator";
 
 test("it adds 1 and 1 and returns 2", () => {
   // arrange
@@ -8,4 +8,13 @@ test("it adds 1 and 1 and returns 2", () => {
 
   // act and assert
   expect(add(param1, param2)).toEqual(expected);
+});
+
+test("it should throw an error when divided by zero", () => {
+  // arrange
+  const param1 = 1;
+  const param2 = 0;
+
+  // act and assert
+  expect(() => divide(param1, param2)).toThrow("Can't divide by zero");
 });
